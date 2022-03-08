14bdd9428358e9fcdfacbdb3c7b6f1291a248e45
Dancho <ddancho@gmail.com>
2022-03-08 15:53:19 +0100

    build for dev or production

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
+let mode = "development";
+
+if (process.env.NODE_ENV === "production") {
+  mode = "production";
+}
+
 module.exports = {
+  mode: mode,
+
   module: {
     rules: [
       {
